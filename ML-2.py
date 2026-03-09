00cf646037e0a7b8f65b69cda857044a7f28ff1f
Jahnavi0960 <prudhvijahnavidevineni@gmail.com>
2026-03-09 14:32:23 +0530

    Add files via upload

---
+++ ML-2.py
@@ -0,0 +1,73 @@
+# 9. GRAPH 2: Simple Regression Visualization (RM vs MEDV)
+# ===============================
+plt.figure()
+plt.scatter(df["RM"], df["MEDV"])
+plt.plot(df["RM"], model.predict(df[["RM", "LSTAT", "PTRATIO", "TAX"]]), linewidth=2)
+plt.xlabel("Average Number of Rooms (RM)")
+plt.ylabel("House Price (MEDV)")
+plt.title("House Price vs Number of Rooms")
+plt.show()
+
+
+
+# Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+# Load dataset (use r before path)
+data = pd.read_csv(r"C:\Users\HP\Documents\Mall_Customers.csv")
+
+print("First 5 rows of dataset:")
+print(data.head())
+
+# Select Annual Income & Spending Score
+X = data.iloc[:, [3, 4]].values
+
+# -------------------------------
+# ELBOW METHOD
+# -------------------------------
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+# Plot Elbow Graph
+plt.figure()
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# -------------------------------
+# APPLY KMEANS (k=5)
+# -------------------------------
+kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
+y_kmeans = kmeans.fit_predict(X)
+
+# -------------------------------
+# VISUALIZATION
+# -------------------------------
+plt.figure()
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, label='Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, label='Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, label='Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s=100, label='Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s=100, label='Cluster 5')
+
+# Centroids
+plt.scatter(kmeans.cluster_centers_[:, 0],
+            kmeans.cluster_centers_[:, 1],
+            s=300,
+            label='Centroids')
+
+plt.title('Customer Segmentation')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
